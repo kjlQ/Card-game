30b3b1792c7effbe95a15b4751e998d3ef21f8a5
Tertychnyi <rett8716@gmail.com>
2022-09-10 18:04:11 +0300

    add logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import backCard from '../images/backcard.png';
-import helmet from '../images/helmet-1.png';
 
-const Card = () => {
+const Card = ({item,setEqual,matched,equal}) => {
+    const [flipped,setFlipped] = useState(false)
+    const imgSrc = require(`../images/${item}-1.png`)
+    const handleClick = () => {
+        setFlipped(true)
+        setEqual(prev=>[...prev,item])
+    }
+    useEffect(()=> {
+        equal[1] && matched.map(match=>match!==item && setFlipped(false))
+    },[matched,equal])
     return (
-        <div className="card">
-            <div className="card__face card__face--front">
+        <div className={`card ${flipped && 'flipped'}`}>
+            <div className="card__face card__face--front" onClick={()=>handleClick()}>
                 <img src={backCard} alt=""/>
             </div>
-            <div className="card__face card__face--back">
-                <img src={helmet} alt=""/>
-
+            <div className={`card__face card__face--back`}>
+                <img src={imgSrc} alt=""/>
             </div>
         </div>
     );
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Card from "./Card";
 
 const Cards = () => {
+    const cards = [
+        "helmet",
+        "helmet",
+        "potion",
+        "potion",
+        "ring",
+        "ring",
+        "scroll",
+        "scroll",
+        "shield",
+        "shield",
+        "sword",
+        "sword"
+    ];
+    const [matched , setMatched] = useState([])
+    const [equal , setEqual] = useState([])
+    useEffect(()=> {
+        setTimeout(()=> {
+            if(equal[0] && equal[0] === equal[1]) {
+                setMatched([...matched,equal[0]])
+            }
+            if(equal.length>1) {
+                setEqual([])
+            }
+        },200)
+    },[equal])
+
+    console.log('matched=>',matched)
+    console.log('equal=>',equal)
+
     return (
         <div className="cards">
             <div className="cards-container">
-                {[...new Array(12)].map(item=><Card />)}
+                {cards.map((item,i)=><Card key={i} matched={matched} equal={equal} item={item} setEqual={setEqual}/>)}
             </div>
         </div>
     );
